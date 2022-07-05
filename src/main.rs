de14b37ac6ef8fd967236f99f980fb70f28acea0
Adriannic <spamadriannic@gmail.com>
2022-07-05 20:38:43 +0200

    Added ability to move your pieces and a visual effect to represent the selected piece

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ const BOARD_SIZE: u32 = 8;
 const TILE_SIZE: f32 = WINDOW_SIZE / BOARD_SIZE as f32;
 
 #[derive(Clone, Copy)]
-enum PieceRoles {
+enum Roles {
     Pawn,
     Rook,
     Knight,
@@ -16,7 +16,7 @@ enum PieceRoles {
     King,
 }
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, PartialEq)]
 enum Teams {
     White,
     Black,
@@ -24,44 +24,44 @@ enum Teams {
 
 #[derive(Clone, Copy)]
 struct Piece {
-    role: PieceRoles,
+    role: Roles,
     team: Teams,
 }
 
 impl Piece {
     fn pawn(team: Teams) -> Piece {
         Piece {
-            role: PieceRoles::Pawn,
+            role: Roles::Pawn,
             team,
         }
     }
     fn rook(team: Teams) -> Piece {
         Piece {
-            role: PieceRoles::Rook,
+            role: Roles::Rook,
             team,
         }
     }
     fn knight(team: Teams) -> Piece {
         Piece {
-            role: PieceRoles::Knight,
+            role: Roles::Knight,
             team,
         }
     }
     fn bishop(team: Teams) -> Piece {
         Piece {
-            role: PieceRoles::Bishop,
+            role: Roles::Bishop,
             team,
         }
     }
     fn queen(team: Teams) -> Piece {
         Piece {
-            role: PieceRoles::Queen,
+            role: Roles::Queen,
             team,
         }
     }
     fn king(team: Teams) -> Piece {
         Piece {
-            role: PieceRoles::King,
+            role: Roles::King,
             team,
         }
     }
@@ -102,8 +102,12 @@ impl Default for Board {
     }
 }
 
+struct Selected(Option<(u32, u32)>);
+
+struct Turn(Teams);
+
 #[derive(Component)]
-struct PieceTag {}
+struct RemovalTag {}
 
 fn main() {
     App::new()
@@ -118,8 +122,11 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .insert_resource(ClearColor(Color::rgb(0.1, 0.1, 0.1)))
         .insert_resource(Board::default())
+        .insert_resource(Selected(None))
+        .insert_resource(Turn(Teams::White))
         .add_startup_system(setup)
         .add_system(draw_pieces)
+        .add_system(move_piece)
         .run();
 }
 
@@ -142,7 +149,7 @@ fn setup(mut commands: Commands) {
                     custom_size: Some(Vec2::new(TILE_SIZE, TILE_SIZE)),
                     ..default()
                 },
-                transform: Transform::from_xyz(coords(i), coords(j), 1.0),
+                transform: Transform::from_xyz(board_to_screen(i), board_to_screen(j), 1.0),
                 ..default()
             });
         }
@@ -151,10 +158,31 @@ fn setup(mut commands: Commands) {
 
 // Systems
 
-fn draw_pieces(mut commands: Commands, asset_server: Res<AssetServer>, board: Res<Board>, pieces: Query<Entity, With<PieceTag>>) {
+fn draw_pieces(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    board: Res<Board>,
+    selected: Res<Selected>,
+    pieces: Query<Entity, With<RemovalTag>>,
+) {
     for entity in pieces.iter() {
         commands.entity(entity).despawn();
     }
+
+    if let Some((x, y)) = selected.0 {
+        commands
+            .spawn_bundle(SpriteBundle {
+                sprite: Sprite {
+                    color: Color::rgba(0.0, 1.0, 0.0, 0.25),
+                    custom_size: Some(Vec2::new(TILE_SIZE, TILE_SIZE)),
+                    ..default()
+                },
+                transform: Transform::from_xyz(board_to_screen(x), board_to_screen(y), 1.5),
+                ..default()
+            })
+            .insert(RemovalTag {});
+    }
+
     for x in 0..BOARD_SIZE {
         for y in 0..BOARD_SIZE {
             match board.0[y as usize][x as usize] {
@@ -163,11 +191,50 @@ fn draw_pieces(mut commands: Commands, asset_server: Res<AssetServer>, board: Re
                     commands
                         .spawn_bundle(SpriteBundle {
                             texture: asset_server.load(get_path(piece).as_str()),
-                            transform: Transform::from_xyz(coords(x), coords(y), 2.0),
+                            transform: Transform::from_xyz(
+                                board_to_screen(x),
+                                board_to_screen(y),
+                                2.0,
+                            ),
                             ..default()
                         })
-                        .insert(PieceTag {});
+                        .insert(RemovalTag {});
                 }
+            }
+        }
+    }
+}
+
+fn move_piece(
+    mut board: ResMut<Board>,
+    mut selected: ResMut<Selected>,
+    turn: Res<Turn>,
+    windows: Res<Windows>,
+    mouse_input: Res<Input<MouseButton>>,
+) {
+    let (x, y) = match windows
+        .get_primary()
+        .expect("No primary window")
+        .cursor_position()
+    {
+        None => return,
+        Some(pos) => cursor_to_board(pos),
+    };
+
+    if mouse_input.just_pressed(MouseButton::Left) {
+        match selected.0 {
+            None => match board.0[y as usize][x as usize] {
+                None => return,
+                Some(piece) => {
+                    if piece.team == turn.0 {
+                        selected.0 = Some((x, y));
+                    }
+                }
+            },
+            Some(pos) => {
+                board.0[y as usize][x as usize] = board.0[pos.1 as usize][pos.0 as usize];
+                board.0[pos.1 as usize][pos.0 as usize] = None;
+                selected.0 = None;
             }
         }
     }
@@ -175,8 +242,16 @@ fn draw_pieces(mut commands: Commands, asset_server: Res<AssetServer>, board: Re
 
 // Auxiliary functions
 
-fn coords(pos: u32) -> f32 {
+fn board_to_screen(pos: u32) -> f32 {
     (WINDOW_SIZE / -2.0) + (pos as f32 + 0.5) * TILE_SIZE
+}
+
+fn screen_to_board(pos: f32) -> u32 {
+    (pos / TILE_SIZE) as u32
+}
+
+fn cursor_to_board(pos: Vec2) -> (u32, u32) {
+    (screen_to_board(pos.x), screen_to_board(pos.y))
 }
 
 fn is_even(n: u32) -> bool {
@@ -189,12 +264,12 @@ fn get_path(piece: Piece) -> String {
         Teams::White => "white",
     };
     let role = match piece.role {
-        PieceRoles::Pawn => "pawn",
-        PieceRoles::Rook => "rook",
-        PieceRoles::Knight => "knight",
-        PieceRoles::Bishop => "bishop",
-        PieceRoles::Queen => "queen",
-        PieceRoles::King => "king",
+        Roles::Pawn => "pawn",
+        Roles::Rook => "rook",
+        Roles::Knight => "knight",
+        Roles::Bishop => "bishop",
+        Roles::Queen => "queen",
+        Roles::King => "king",
     };
     format!("sprites/{}_{}.png", role, team)
 }
